bdf915ae9b1e4ea8f49eb5dc823157f33c7170be
ArbyPangestu <arbypangestu340@gmail.com>
2025-06-02 15:14:48 +0700

    Menambahkan derivedClass dari baseClass final

---
+++ noOverriding.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class baseclass {
+    public:
+        virtual void perkenalan() {
+            cout << "Hallo, saya function dari base class\n";
+        }
+    };
+
+class derivedclass : public baseclass {
+    public:
+        void perkenalan() final {
+            cout << "Hallo, saya function dari derived class\n";
+        }
+    };
+
+int main() {
+    derivedclass a;
+    a.perkenalan();
+
+    return 0;
+}
